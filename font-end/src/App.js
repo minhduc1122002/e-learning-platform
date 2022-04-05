129718a2ffbb7e70eeaf1b6ec56832e2c4e1ea99
Khanh Linh <khanhlinh.ph2002@gmail.com>
2022-04-05 17:05:26 +0700

    add route to profile

--- font-end/src/App.js
+++ font-end/src/App.js
@@ -23,6 +23,7 @@ function App() {
           <Route path="/test" element={<Test/>} />
           <Route path="/login" element={user ? <Navigate to="/"/> : <Login/>} />
           <Route path = "/signup" element = {user ? <Navigate to="/"/> : <Signup/>} />
+          <Route path = "/profile" element = {<Profile />} />
         </Routes>
       </Router>
     );
